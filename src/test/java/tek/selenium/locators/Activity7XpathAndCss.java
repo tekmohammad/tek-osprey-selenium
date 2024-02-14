2e6c21ac1f4c076637deecaef90f218208d9afa7
mohammad <mohammad@tekschool.us>
2024-02-13 20:13:36 -0500

    Selenium Day 5

---
+++ src/test/java/tek/selenium/locators/Activity7XpathAndCss.java
@@ -0,0 +1,8 @@
+package tek.selenium.locators;
+
+public class Activity7XpathAndCss {
+    /*
+    In Retail App Click on Computer section, then click
+    on First Keyboard and print that title. 
+     */
+}
